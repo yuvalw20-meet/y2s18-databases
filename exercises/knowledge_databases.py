8e937823a9e0a93556fe278a5fca12d93c4a197a
myname21-meet <myname21@meet.mit.edu>
2019-07-29 09:00:45 -0400

    startes lab

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,17 +8,42 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(name, article, topic, article_rating):
+	knowledge_object = Knowledge(
+	    name = name,
+	    article = article,
+	    topic = topic,
+	    article_rating = article_rating)
+
+	session.add(knowledge_object)
+	session.commit()
+
+add_article(name = "Kareem",article = "https://en.wikipedia.org/wiki/Biology",topic = "Biology",article_rating = 2)
+add_article(name = "Wissam",article = "https://en.wikipedia.org/wiki/Cat#Disease",topic = "Killing Cats",article_rating = 10)
+add_article(name = "Caleb",article = "https://en.wikipedia.org/wiki/Bhangra_(dance)",topic = "Banghra Dancing",article_rating = 8)
+
+
 
 def query_all_articles():
-	pass
+	all_articles = session.query(Knowledge).all()
+	print(all_articles)
+	return
 
-def query_article_by_topic():
-	pass
+print(query_all_articles())
+
+def query_article_by_topic(topic):
+	artcile_by_topic = session.query(Knowledge).filter_by(topic=topic).first()
+	print(artcile_by_topic)
+	return
+
+query_article_by_topic("Biology")
+
 
 def delete_article_by_topic():
-	pass
+	delete_by_topic = session.query(Knowledge).filter_by(topic=topic).delete()
+	session.commit()
+
+delete_article_by_topic("Banghra Dancing")
 
 def delete_all_articles():
 	pass
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -6,10 +6,36 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
-	tablename__ = 'peopleWannaLearn'
+	__tablename__ = 'peopleWannaLearn'
 	student_id = Column(Integer, primary_key=True)
+	name = Column(String)
 	article = Column(String)
 	topic = Column(String)
-	article = Column(Integer)
+	article_rating = Column(Integer)
+	def __repr__(self):
+		consReturn =("ID: {}\n"
+				"Student Name: {}\n"
+				"Chosen Topic: {}\n"
+				"Chosen Article: {}\n"
+				"Chosen article_rating: {}").format(
+					self.student_id,
+					self.name,
+					self.topic,
+					self.article,
+					self.article_rating,)
 
-	pass
+		if self.article_rating < 7:
+			return(str(consReturn) + ", not very useful is it...")
+		else:
+			return(str(consReturn)) 
+
+
+
+# kareem = Knowledge(student_id = 0,name = "Kareem",article = "https://en.wikipedia.org/wiki/Biology",topic = "Biology",article_rating = 2)
+# wissam = Knowledge(student_id = 1,name = "Wissam",article = "https://en.wikipedia.org/wiki/Cat#Disease",topic = "Killing Cats",article_rating = 10)
+# caleb = Knowledge(student_id = 2,name = "Caleb",article = "https://en.wikipedia.org/wiki/Bhangra_(dance)",topic = "Banghra Dancing",article_rating = 8)
+
+
+
+
+# print(str(kareem) + "\n\n" + str(wissam) + "\n\n" + str(caleb))
